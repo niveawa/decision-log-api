bbc36f87ac9fa962617aa899d909689450c47dd1
Nivea <nivanina.martins@gmail.com>
2025-12-19 13:02:13 -0300

    chore: initial Spring Boot setup with PostgreSQL and Flyway

---
+++ src/main/java/com/nivea/decisionlog/DecisionlogApplication.java
@@ -0,0 +1,13 @@
+package com.nivea.decisionlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nivea/decisionlog/DecisionlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivea.decisionlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
